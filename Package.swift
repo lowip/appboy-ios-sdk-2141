f83f5acd80936058d0c754b4c54c3383653c5704
Louis Bur <louisbur92@gmail.com>
2021-12-20 11:39:41 +0100

    first commit

---
+++ Package.swift
@@ -0,0 +1,66 @@
+// swift-tools-version:5.3
+
+import PackageDescription
+
+let package = Package(
+  name: "Appboy_iOS_SDK",
+  defaultLocalization: "en",
+  platforms: [
+    .iOS(.v9)
+  ],
+  products: [
+    .library(name: "AppboyKit", type: .static, targets: ["AppboyKit"]),
+    .library(name: "AppboyUI", targets: ["AppboyUI"]),
+    .library(name: "AppboyPushStory", targets: ["AppboyPushStory"])
+  ],
+  dependencies: [
+    .package(name: "SDWebImage", url: "https://github.com/SDWebImage/SDWebImage.git", from: "5.8.2")
+  ],
+  targets: [
+    .binaryTarget(
+      name: "AppboyKitLibrary",
+      path: "AppboyKit/AppboyKitLibrary.xcframework"
+    ),
+    .target(
+      name: "AppboyKit",
+      dependencies: ["SDWebImage", "AppboyKitLibrary"],
+      path: "AppboyKit",
+      resources: [
+        .process("Appboy.bundle")
+      ],
+      linkerSettings: [
+        .linkedFramework("SystemConfiguration"),
+        .linkedFramework("QuartzCore"),
+        .linkedFramework("CoreImage"),
+        .linkedFramework("CoreText"),
+        .linkedFramework("WebKit"),
+        .linkedFramework("UserNotifications"),
+        .linkedFramework("CoreTelephony", .when(platforms: [.iOS])),
+        .linkedLibrary("z"),
+      ]
+    ),
+    .target(
+      name: "AppboyUI",
+      dependencies: ["AppboyKit"],
+      path: "AppboyUI",
+      resources: [
+        .process("ABKNewsFeed/Resources"),
+        .process("ABKInAppMessage/Resources"),
+        .process("ABKContentCards/Resources")
+      ],
+      publicHeadersPath: "include/AppboyUI"
+    ),
+    .binaryTarget(
+      name: "AppboyPushStoryFramework",
+      path: "AppboyPushStory/AppboyPushStoryFramework.xcframework"
+    ),
+    .target(
+      name: "AppboyPushStory",
+      dependencies: ["AppboyPushStoryFramework"],
+      path: "AppboyPushStory",
+      resources: [
+        .process("Resources/ABKPageView.nib")
+      ]
+    )
+  ]
+)
