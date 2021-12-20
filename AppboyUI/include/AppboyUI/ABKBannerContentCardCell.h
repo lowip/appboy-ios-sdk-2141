4ad84d62043b6a60e45b36a932bff87cd76e58a1
Louis Bur <louisbur92@gmail.com>
2021-12-20 12:15:23 +0100

    Fix headers

--- AppboyUI/include/AppboyUI/ABKBannerContentCardCell.h
+++ AppboyUI/include/AppboyUI/ABKBannerContentCardCell.h
@@ -1,11 +1,1 @@
-#import "ABKBaseContentCardCell.h"
-#import "ABKBannerContentCard.h"
-
-@interface ABKBannerContentCardCell : ABKBaseContentCardCell
-
-@property (strong, nonatomic) IBOutlet UIImageView *bannerImageView;
-@property (strong, nonatomic) IBOutlet NSLayoutConstraint *imageRatioConstraint;
-
-- (void)applyCard:(ABKBannerContentCard *)bannerCard;
-
-@end
+../../ABKContentCards/ViewControllers/Cells/ABKBannerContentCardCell.h
--- AppboyUI/include/AppboyUI/ABKCaptionedImageContentCardCell.h
+++ AppboyUI/include/AppboyUI/ABKCaptionedImageContentCardCell.h
@@ -1,18 +1,1 @@
-#import "ABKBaseContentCardCell.h"
-#import "ABKCaptionedImageContentCard.h"
-
-@interface ABKCaptionedImageContentCardCell : ABKBaseContentCardCell
-
-@property (class, nonatomic) UIColor *titleLabelColor;
-@property (class, nonatomic) UIColor *descriptionLabelColor;
-@property (class, nonatomic) UIColor *linkLabelColor;
-
-@property (strong, nonatomic) IBOutlet UIImageView *captionedImageView;
-@property (strong, nonatomic) IBOutlet NSLayoutConstraint *imageRatioConstraint;
-@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
-@property (strong, nonatomic) IBOutlet UILabel *descriptionLabel;
-@property (strong, nonatomic) IBOutlet UILabel *linkLabel;
-
-- (void)applyCard:(ABKCaptionedImageContentCard *)captionedImageCard;
-
-@end
+../../ABKContentCards/ViewControllers/Cells/ABKCaptionedImageContentCardCell.h
